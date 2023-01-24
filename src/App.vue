1ddbc8177955a3b08a2eb141887b37e0142a449a
moezbenrebah <benrebah.moez@gmail.com>
2023-01-24 12:00:43 +0100

    starter code

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="content">
+    <nav>
+      <router-link to="/">Home</router-link>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+<style></style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="home">
+    <p>Home Page</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+};
+</script>
