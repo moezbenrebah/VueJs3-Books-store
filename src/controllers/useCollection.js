5a19a963db255527fc1eb5142ab11ba3c6c71c5f
moezbenrebah <benrebah.moez@gmail.com>
2023-03-15 15:45:53 +0100

    update useCollection API & update webpack dependency

--- src/controllers/useCollection.js
+++ src/controllers/useCollection.js
@@ -1,10 +1,7 @@
 import { ref } from "vue";
 import { db } from "../firebase/firebaseConfig";
 
-const isLoading = ref(false);
-
 const useCollection = (collection) => {
-  isLoading.value = true;
   // reset the error value at the start of every request
   const error = ref(null);
 
@@ -13,16 +10,14 @@ const useCollection = (collection) => {
     error.value = null;
     try {
       const response = await db.collection(collection).add(doc);
-      isLoading.value = false;
       return response;
     } catch (err) {
       console.log(err.message);
       error.value = "could not send the message";
-      isLoading.value = false;
     }
   };
 
-  return { error, addDoc, isLoading };
+  return { error, addDoc };
 };
 
 export default useCollection;
